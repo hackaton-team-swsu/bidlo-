4c6374f16d2c5f3489d78a36e6f28abc16f346ed
moonwtf <gfrimanr@gmail.com>
2019-12-01 21:30:59 +0300

    развлекайтесь

---
+++ hui/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#define хуй cout
+#define ну_бля if
+#define внатуре true
+#define погнали_нахуй {
+#define ёбаный_рот }
+#define ёпта (
+#define бля )
+#define сука ;
+#define ебаный <<
+#define ыэуа int
+#define еблысь return
+#define нахуй void
+
+using namespace std;
+
+нахуй getSum ёпта ыэуа a, ыэуа b бля погнали_нахуй
+	хуй ебаный a + b сука
+ёбаный_рот
+
+
+ыэуа main ёпта бля
+погнали_нахуй
+
+	ну_бля ёпта внатуре бля погнали_нахуй
+		getSum ёпта 1, 2 бля сука
+	ёбаный_рот
+
+ёбаный_рот
+
